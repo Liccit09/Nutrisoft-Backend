c6af12caa0896dacbaa3bed67b17f05173477130
Liccit Moncada <testerqaliccitmoncada@gmail.com>
2026-04-28 07:31:50 +0200

    Rename CORS configuration package to align with user interface API structure

--- src/main/java/com/nutrisoft/userinterface/api/rest/config/CorsConfig.java
+++ src/main/java/com/nutrisoft/userinterface/api/rest/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.nutrisoft.infrastructure.config;
+package com.nutrisoft.userinterface.api.rest.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
